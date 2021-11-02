9c433689e61c6f97647b2ff37f2bde9b6fe5f48a
Daniel Pascual López <uo269728@uniovi.es>
2021-11-02 16:54:53 +0100

    asefd

--- practica2/Ejercicio-3/getXAMLcomponets.py
+++ practica2/Ejercicio-3/getXAMLcomponets.py
@@ -3,34 +3,34 @@
 import xml.etree.ElementTree as ET
 
 
-
-
-def print_seguro(xml, dire, name):
-    try:
-        temp = xml.find(dire).text
-        print('\t-> %s: %s' % (name, temp))
-    except AttributeError:
-        return
-
-    return
-
 def parse_locations(r):
-                root = r
-                print('=> Componentes del XAML:')
-                componentes = root.find("{http://schemas.microsoft.com/winfx/2006/xaml/presentation}Grid")
-                for componente in componentes:
-                    print('\t-Componente: '+componente.tag.replace("{http://schemas.microsoft.com/winfx/2006/xaml/presentation}",'') +', con una dispocicion:')
-                    print( '\t\tHorizontalAlignment:' +componente.get('HorizontalAlignment'))
-                    print( '\t\tVerticalAlignment:' +componente.get('VerticalAlignment'))
-                    print( '\t\tMargin:' +componente.get('Margin'))
-                return
+    text = ""
+    root = r
+    text+='=> Componentes del XAML:\n'
+    componentes = root.find(
+        "{http://schemas.microsoft.com/winfx/2006/xaml/presentation}Grid")
+    for componente in componentes:
+        text+='\t-Componente: '+componente.tag.replace(
+            "{http://schemas.microsoft.com/winfx/2006/xaml/presentation}", '') + ', con una dispocicion:\n'
+        text+='\t\tHorizontalAlignment:' + componente.get('HorizontalAlignment')+"\n"
+        text+='\t\tVerticalAlignment:' + componente.get('VerticalAlignment')+"\n"
+        text+='\t\tMargin:' + componente.get('Margin')+"\n"
+    return text
+
+
+def saveToFile(text):
+    text_file = open("Output.txt", "w")
+    text_file.write(text)
+    text_file.close()
 
 
 def main():
     filename = "MainWindow.xaml"
     document = ET.parse(filename)
     root = document.getroot()
-    parse_locations(root)
+    text = parse_locations(root)
+    saveToFile(text)
+
 
 if __name__ == "__main__":
     main()

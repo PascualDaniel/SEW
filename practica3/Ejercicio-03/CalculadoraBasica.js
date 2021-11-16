79c6a31e4bd989120b8eb5df3249106dfeea30cb
Daniel Pascual López <uo269728@uniovi.es>
2021-11-16 12:53:03 +0100

    asfvn

--- practica3/Ejercicio-03/CalculadoraBasica.js
+++ practica3/Ejercicio-03/CalculadoraBasica.js
@@ -1,66 +1,73 @@
 "use strict";
 class Calculadora {
-    constructor(){
-    	this.vPantalla = "0";//valor de la pantalla actual
-    	this.memoria="0";
-	}
-    addElement(x) {
-        if(this.vPantalla == "0"){
-            this.vPantalla = x;
-        }else{
-         this.vPantalla += x;
-        }
-        document.getElementById('pantalla').value=this.vPantalla;
-    }
-    calc(){
-         try{
-             //Solucion1
+    constructor() {
+        this.vPantalla = "0";//valor de la pantalla actual
+        this.memoria = "0";
 
-             let tokens =this.tokenize(this.vPantalla);
-          //  print(tokens);
+        document.addEventListener("keypress", function (e) {
+            calculadora.onKeyPress(e);
+        });
+
+    }
+    addElement(x) {
+        if (this.vPantalla == "0") {
+            this.vPantalla = x;
+        } else {
+            this.vPantalla += x;
+        }
+        document.getElementById('pantalla').value = this.vPantalla;
+    }
+    calc() {
+        try {
+            //Solucion1
+
+            let tokens = this.tokenize(this.vPantalla);
+            //print(tokens);
             this.vPantalla = eval(tokens.join(''));
 
-            document.getElementById('pantalla').value =this.vPantalla;
-         }catch(err){
-            this.vPantalla = "Error: "+err;
-            document.getElementById('pantalla').value =this.vPantalla;
+            document.getElementById('pantalla').value = this.vPantalla;
+        } catch (err) {
+            this.vPantalla = "Error: " + err;
+            document.getElementById('pantalla').value = this.vPantalla;
         }
     }
-    limpiar(){
-        try{    
+    limpiar() {
+        try {
             this.vPantalla = "0";
-            document.getElementById('pantalla').value =this.vPantalla;
-        }catch(err){
-            this.vPantalla = "Error: "+err;
-            document.getElementById('pantalla').value =this.vPantalla;
+            document.getElementById('pantalla').value = this.vPantalla;
+        } catch (err) {
+            this.vPantalla = "Error: " + err;
+            document.getElementById('pantalla').value = this.vPantalla;
         }
     }
     //M+
-    sumarMemoria(){
-        try{    
-            this.memoria = eval(this.memoria+" + "+ this.vPantalla);
+    sumarMemoria() {
+        try {
+            this.calc();
+            this.memoria = new Number(eval(this.memoria + " + " + new Number(this.vPantalla)));
 
-        }catch(err){
-            this.vPantalla = "Error: "+err;
-            document.getElementById('pantalla').value =this.vPantalla;
+        } catch (err) {
+            this.vPantalla = "Error: " + err;
+            document.getElementById('pantalla').value = this.vPantalla;
         }
     }
     //M-
-    restarMemoria(){
-        try{    
-            this.memoria = eval(this.memoria+" - "+ this.vPantalla);
+    restarMemoria() {
+        try {
+            this.calc();
+            this.memoria = new Number(eval(this.memoria + " - " + new Number(this.vPantalla)));
 
-        }catch(err){
-            this.vPantalla = "Error: "+err;
-            document.getElementById('pantalla').value =this.vPantalla;
+        } catch (err) {
+            this.vPantalla = "Error: " + err;
+            document.getElementById('pantalla').value = this.vPantalla;
         }
     }
     //MRC
-    sacarMemoria(){
+    sacarMemoria() {
 
-        this.vPantalla = ""+ this.memoria;
-        document.getElementById('pantalla').value =this.vPantalla;
-         
+        this.vPantalla = "" + this.memoria;
+        document.getElementById('pantalla').value = this.vPantalla;
+
     }
 
     tokenize(text) {
@@ -85,7 +92,57 @@ class Calculadora {
         console.log(tokens);
         return tokens;
     }
-    
+
+   
+
+
+    //on keyboardEvent call function
+    onKeyPress(keyboardEvent) {
+        //console.log(keyboardEvent.key);
+        switch (keyboardEvent.key) {
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+            case '.':
+                this.addElement(keyboardEvent.key);
+                break;
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+                this.addElement(keyboardEvent.key);
+                break;
+            case 'Enter':
+                this.calc();
+                break;
+            case 'backspace':
+                this.limpiar();
+                break;
+            case 'b':
+                this.sumarMemoria();
+                break;
+            case 'n':
+                this.restarMemoria();
+                break;
+            case 'm':
+                this.sacarMemoria();
+                break;
+            case 'c':
+                this.limpiar();
+                break;
+        }
+    }
+
+       
+
+
 }
 
 var calculadora = new Calculadora();
--- practica3/Ejercicio-10/Ejercicio10.js
+++ practica3/Ejercicio-10/Ejercicio10.js
@@ -3,19 +3,46 @@
 
 "use strict";
 class Bitcoin {
-   
+    constructor() {
+        this.dollars;//valor de la pantalla actual
+        this.state = true;
+    }
     //obtain bitcoin data using ajax
-    obtainData(){
+    obtainData() {
         let xhr = new XMLHttpRequest();
         xhr.open('GET', 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json');
         xhr.send();
-        xhr.onreadystatechange = function() {
+        xhr.onreadystatechange = function () {
             if (xhr.readyState == 4 && xhr.status == 200) {
                 let response = JSON.parse(xhr.responseText);
-                document.querySelector('main p').innerHTML = response.bpi.USD.rate;
+                document.querySelector('main p').innerHTML = response.bpi.USD.rate + "$";;
             }
         }
     }
+    //change the value of the dollars to euros
+    changeDollars() {
+        if (this.state == true) {
+            this.dollars = document.querySelector('main p').innerHTML;
+            this.dollars = this.dollars.replace('$', '');
+            this.dollars = parseFloat(this.dollars);
+            this.dollars = this.dollars * 0.88;
+            document.querySelector('main p').innerHTML = this.dollars + "€";
+            this.state = false;
+        }
+    }
+    //change the value of the euros to dollars
+    changeEuros() {
+        if (this.state == false) {
+            this.dollars = document.querySelector('main p').innerHTML;
+            this.dollars = this.dollars.replace('€', '');
+            this.dollars = parseFloat(this.dollars);
+            this.dollars = this.dollars / 0.88;
+            document.querySelector('main p').innerHTML = this.dollars + "$";
+            this.state = true;
+        }
+    }
+
+
 
 }
 var bit = new Bitcoin();

f456d2287ac6a339aef87e39138a05166c6ffc60
Daniel Pascual López <uo269728@uniovi.es>
2021-11-28 02:29:35 +0100

    uhuj

---
+++ practica3/Ejercicio-01/Cabecera.js
@@ -0,0 +1,11 @@
+// Cabecera.js
+// Informacion de asignatura
+// Version 1.0. Daniel Pascual Lopez. Universidad de Oviedo
+var asignatura = new Object();
+asignatura.nombre = "Software y estándares en la Web";
+asignatura.titulacion = "Grado en Ingeniería Informática del Software";
+asignatura.centro = "Escuela de Ingeniería Informática";
+asignatura.universidad = "Universidad de Oviedo";
+asignatura.curso = "2020-21";
+asignatura.estudiante = "Daniel Pascual Lopez";
+asignatura.email = "uo269728@uniovi.es";
